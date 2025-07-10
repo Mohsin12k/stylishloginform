8ed5768a9ef072c81f04ad3fdc2bb6ff6310cc76
Mohsin <mk773404@gmail.com>
2025-07-10 13:15:47 +0500

    js modified

--- main.js
+++ main.js
@@ -4,28 +4,20 @@ const closeIcon = document.querySelector('.closeIcon');
 const registerMe = document.querySelector('.registerNow');
 const accountExist = document.querySelector('.accountExist');
 
-// Toggle popup
-popupButton.addEventListener('click', () => {
+popupButton.addEventListener('click', ()=>{
     mainForm.classList.toggle('popup_btn');
-    if (mainForm.classList.contains('popup_btn')) {
-        mainForm.style.transform = 'scale(1)';
-    } else {
-        mainForm.style.transform = 'scale(0)';
-    }
+    mainForm.classList.remove('active');
 });
 
-// Close icon
-closeIcon.addEventListener('click', () => {
+closeIcon.addEventListener('click', ()=>{
     mainForm.classList.remove('popup_btn');
-    mainForm.style.transform = 'scale(0)';
+    mainForm.classList.remove('active');
 });
 
-// Switch to registration
-registerMe.addEventListener('click', () => {
+registerMe.addEventListener('click', ()=>{
     mainForm.classList.add('active');
 });
 
-// Switch to login
-accountExist.addEventListener('click', () => {
+accountExist.addEventListener('click', ()=>{
     mainForm.classList.remove('active');
-});
+})
